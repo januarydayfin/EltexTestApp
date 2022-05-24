f6aeca50ff1e968dfd9905119ca0c41b16ba2457
januarydayfin <krayman7250@gmail.com>
2022-05-24 11:27:09 +0700

    Initial commit

---
+++ app/src/main/java/com/krayapp/eltextestapp/EltexApp.java
@@ -0,0 +1,10 @@
+package com.krayapp.eltextestapp;
+
+import android.app.Application;
+
+public class EltexApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
